e2f0e0c8567c373f06e902904afb412dc5aed93b
hongduck710 <hongik710@gmail.com>
2024-12-07 17:36:42 +0900

    파이어베이스를 이용한 회원가입까지 완료

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {db} from "../firebase.config";
 import {Link, useNavigate} from "react-router-dom";
 import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilidtyIcon from "../assets/svg/visibilityIcon.svg";
@@ -22,13 +24,28 @@ function SignUp(){
         }));
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            updateProfile(auth.currentUser, {
+                displayName: name
+            });
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="pageContainer">
                 <header>
                     <p>어서오세요!</p>
                 </header>
-                <form>
+                <form onSubmit={onSubmit}>
                     <input 
                     type="text" 
                     className="nameInput"
