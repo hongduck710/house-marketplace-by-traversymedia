c8a55979f1f44466711944aa077795882857b55a
hongduck710 <hongik710@gmail.com>
2024-12-20 21:21:38 +0900

    7 - Display & Upadte User Details 4:28까지 진행

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,56 @@
 import {useState, useEffect} from "react";
-import {getAuth} from "firebase/auth";
+import {getAuth, updateProfile} from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
+import {updateDoc} from "firebase/firestore";
+import {db} from "../firebase.config";
 
 function Profile(){
-    const [user, setUser] = useState(null);
-
     const auth = getAuth();
-    useEffect(() => {
-        setUser(auth.currentUser);
-    },[]);
+    const [changeDetails, setChangeDetails] = useState(false);
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email
+    });
 
-    return user ? <h1>{user.displayName}</h1> : "로그인 되지 않음"
+    const {name, email} = formData;
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        auth.signOut();
+        navigate("/")
+    }
+
+    const onSubmit = () => {
+        console.log(123);
+    }
+
+    return <div className="profile">
+        <header className="profileHeader">
+            <p className="pageHeader">My Profile</p>
+            <button type="button" onClick={onLogout} className="logOut">
+                Logout
+            </button>
+        </header>
+        <main>
+            <div className="profileDetailsHeader">
+                <p className="profileDetailsText">
+                    Personal Details
+                </p>
+                <p className="changePersonalDetails" onClick={() => {
+                    changeDetails && onsubmit();
+                    setChangeDetails((prevState) => !prevState)
+                }}>
+                    {changeDetails ? "done" : "change"}
+                </p>
+            </div>
+            <div className="profileCard">
+                <form>
+                    
+                </form>
+            </div>
+        </main>
+    </div>
 }
 
 export default Profile;
