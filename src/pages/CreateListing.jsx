0649a341d8aec1d6a1f82f6488fa705c791cd7f1
hongduck710 <hongik710@gmail.com>
2025-01-06 21:54:54 +0900

    7 - Get Coords With Geocoding API 완료(구글 결제등록으로 인해 api 적용은 하지 않음 )

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -60,7 +60,7 @@ function CreateListing(){
         }
     }, [isMounted]);
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
 
         setLoading(true);
@@ -77,6 +77,32 @@ function CreateListing(){
             toast.error("Max 6 images");
             return;
         }
+
+        let geolocation = {};
+        let location;
+
+        if(geolocationEnabled) {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
+            const data = await response.json();
+
+            geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+            geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+            location = data.status === "ZERO_RESULTS" 
+            ? undefined 
+            : data.results[0]?.formatted_address; 
+
+            if(location === undefined || location.includes("undefined")){
+                setLoading(false);
+                toast.error("Please enter a correct address");
+                return;
+            }
+        } else {
+            geolocation.lat = latitude;
+            geolocation.lng = longitude;
+            location = address;
+            console.log(geolocation, location);
+        }
+        setLoading(false);
     }
 
     const onMutate = (e) => {
