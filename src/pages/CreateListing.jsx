6a92b65e442fceb7ebba99cf0287bdc526526f1a
hongduck710 <hongik710@gmail.com>
2025-01-06 22:09:26 +0900

    8 - Uploading Images To Firebase 5:07까지 진행. (uuid 설치 완료)

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,17 @@
 import {useState, useEffect, useRef} from "react";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL
+} from "firebase/storage";
+import {db} from "../firebase.config";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
+import {v4 as uuidv4} from "uuid";
 import Spinner from "../components/Spinner";
+
 
 
 function CreateListing(){
@@ -102,6 +111,15 @@ function CreateListing(){
             location = address;
             console.log(geolocation, location);
         }
+
+        // Store images in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage();
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+            });
+        }
+
         setLoading(false);
     }
 
