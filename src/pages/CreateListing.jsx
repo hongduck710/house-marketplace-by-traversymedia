9ea13200581081305ec2f22f3c57e25432b0df06
hongduck710 <hongik710@gmail.com>
2025-01-10 20:21:54 +0900

    8 - Uploading Images To Firebase까지 완료

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -86,7 +86,7 @@ function CreateListing(){
             toast.error("Max 6 images");
             return;
         }
-
+/* 구글 위치 api를 사용하기 위해선 결제정보를 입력해야 하기 때문에 주석처리 함
         let geolocation = {};
         let location;
 
@@ -111,14 +111,54 @@ function CreateListing(){
             location = address;
             console.log(geolocation, location);
         }
-
+*/
         // Store images in firebase
         const storeImage = async (image) => {
             return new Promise((resolve, reject) => {
                 const storage = getStorage();
                 const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+                const storageRef = ref(storage, "images/" + fileName);
+
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+                uploadTask.on('state_changed', 
+                    (snapshot) => {
+
+                      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                      console.log('Upload is ' + progress + '% done');
+                      switch (snapshot.state) {
+                        case 'paused':
+                          console.log('Upload is paused');
+                          break;
+                        case 'running':
+                          console.log('Upload is running');
+                          break;
+                      }
+                    }, 
+                    (error) => {
+                      reject(error);
+                    }, 
+                    () => {
+                      // Handle successful uploads on complete
+                      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                      });
+                    }
+                );
             });
         }
+
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false);
+            toast.error("Images not uploaded");
+            return;
+        });
+
+        console.log(imgUrls);
 
         setLoading(false);
     }
