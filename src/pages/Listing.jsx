95668d0010837fb4e91cb6b1c1b8b22428ba0451
hongduck710 <hongik710@gmail.com>
2025-01-18 23:49:16 +0900

    4 - Leaflet Map 완료

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import {Link, useNavigate,useParams} from "react-router-dom";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {getDoc, doc} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 import {db} from "../firebase.config";
@@ -80,17 +81,30 @@ function Listing() {
                     <li>{listing.parking && "Parking Spot"}</li>
                     <li>{listing.furnished && "Furnished"}</li>
                 </ul>
-                <p className="listingLocationTitle">Location
-                    {/* MAP */}
-                    {auth.currentUser?.uid !== listing.userRef && (
-                        <Link to={`/contact/${listing.userRef}?
+                <p className="listingLocationTitle">Location</p>
+                
+                <div className="leafletContainer">
+                    <MapContainer style={{height:"100%", width:"100%"}}
+                    center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer 
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
+                        url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                        />
+                        <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>        
+
+                {auth.currentUser?.uid !== listing.userRef && (
+                    <Link to={`/contact/${listing.userRef}?
                         listingName=${listing.name}`} 
                         className="primaryButton"
                         >
-                            Contact Landlord
-                        </Link>
-                    )}
-                </p>
+                        Contact Landlord
+                    </Link>
+                )}
+                
             </div>
         </main>
 
@@ -98,3 +112,5 @@ function Listing() {
 }
 
 export default Listing;
+
+// https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
